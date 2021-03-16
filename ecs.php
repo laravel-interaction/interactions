ddd0317464da4db8b2c1a8259d17db91868a03d4
zingimmick <zingimmick@outlook.com>
2021-03-16 17:15:24 +0800

    first commit

---
+++ ecs.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitInternalClassFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestClassRequiresCoversFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/vendor/zing/coding-standard/config/config.php');
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(
+        Option::SETS,
+        [
+            SetList::CLEAN_CODE,
+            SetList::COMMON,
+            SetList::PSR_12,
+        ]
+    );
+    $parameters->set(
+        Option::SKIP,
+        [
+            '*/migrations/*',
+            YodaStyleFixer::class => null,
+            PhpUnitInternalClassFixer::class,
+            PhpUnitTestClassRequiresCoversFixer::class,
+            NoSuperfluousPhpdocTagsFixer::class,
+        ]
+    );
+    $parameters->set(
+        Option::PATHS,
+        [
+            __DIR__ . '/packages',
+            __DIR__ . '/changelog-linker.php',
+            __DIR__ . '/ecs.php',
+            __DIR__ . '/rector.php',
+        ]
+    );
+};
