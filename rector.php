caaef309190897bbdc1585009f9d431aaf238b68
Zing <zingimmick@outlook.com>
2021-03-28 15:21:10 +0800

    Apply SetList::SYMPLIFY (#14)
    * Bump rector/rector from 0.9 to 0.10
    
    * Apply SetList::SYMPLIFY

--- rector.php
+++ rector.php
@@ -59,7 +59,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         [
             __DIR__ . '/packages',
             __DIR__ . '/changelog-linker.php',
-            //            __DIR__ . '/ecs.php',
+            __DIR__ . '/ecs.php',
+            __DIR__ . '/monorepo-builder.php',
             __DIR__ . '/rector.php',
         ]
     );
