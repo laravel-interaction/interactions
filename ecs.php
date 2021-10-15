03abc57b370435c588ad04faa730ec54c481a3de
zingimmick <zingimmick@outlook.com>
2021-10-15 11:19:02 +0800

    Add type declaration as default sets

--- ecs.php
+++ ecs.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
-use PhpCsFixer\Fixer\PhpUnit\PhpUnitInternalClassFixer;
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestClassRequiresCoversFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
@@ -21,7 +20,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         [
             '*/migrations/*',
             YodaStyleFixer::class => null,
-            PhpUnitInternalClassFixer::class,
             PhpUnitTestClassRequiresCoversFixer::class,
         ]
     );
