5e091d93ad54e2121c442d1c9fd3acf04fe99ac8
Zing <zingimmick@outlook.com>
2022-05-08 21:14:22 +0800

    Add support for new ECSConfig and RectorConfig class (#121)

--- ecs.php
+++ ecs.php
@@ -2,23 +2,18 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Zing\CodingStandard\Set\ECSSetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(ECSSetList::PHP_73);
-    $containerConfigurator->import(ECSSetList::CUSTOM);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
-    $parameters->set(
-        Option::SKIP,
-        [
-            \PHP_CodeSniffer\Standards\PSR1\Sniffs\Classes\ClassDeclarationSniff::class => ['*/migrations/*'],
-            \PHP_CodeSniffer\Standards\Squiz\Sniffs\PHP\CommentedOutCodeSniff::class,
-        ]
-    );
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([ECSSetList::PHP_73, ECSSetList::CUSTOM]);
+    $parameters = $ecsConfig->parameters();
+    $ecsConfig->parallel();
+    $ecsConfig->skip([
+        \PHP_CodeSniffer\Standards\PSR1\Sniffs\Classes\ClassDeclarationSniff::class => ['*/migrations/*'],
+        \PHP_CodeSniffer\Standards\Squiz\Sniffs\PHP\CommentedOutCodeSniff::class,
+    ]);
     $parameters->set(
         Option::PATHS,
         [__DIR__ . '/packages', __DIR__ . '/ecs.php', __DIR__ . '/monorepo-builder.php', __DIR__ . '/rector.php']
--- rector.php
+++ rector.php
@@ -5,43 +5,32 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use Rector\CodingStyle\Rector\ClassMethod\ReturnArrayClassMethodToYieldRector;
 use Rector\CodingStyle\ValueObject\ReturnArrayClassMethodToYield;
-use Rector\Core\Configuration\Option;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Zing\CodingStandard\Set\RectorSetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(RectorSetList::CUSTOM);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_73);
-
-    $services = $containerConfigurator->services();
-    $services->set(ReturnArrayClassMethodToYieldRector::class)
-        ->configure([new ReturnArrayClassMethodToYield(TestCase::class, '*provide*')]);
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, __DIR__ . '/phpstan.neon');
-    $parameters->set(
-        Option::BOOTSTRAP_FILES,
-        [
-            __DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php',
-            __DIR__ . '/vendor/symplify/easy-coding-standard/vendor/autoload.php',
-            __DIR__ . '/vendor/nunomaduro/larastan/bootstrap.php',
-        ]
-    );
-    $parameters->set(
-        Option::SKIP,
-        [
-            RenameParamToMatchTypeRector::class,
-            AddSeeTestAnnotationRector::class,
-            FinalizeClassesWithoutChildrenRector::class,
-        ]
-    );
-    $parameters->set(
-        Option::PATHS,
+return static function (Rector\Config\RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_73, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
+    $rectorConfig
+        ->ruleWithConfiguration(
+            ReturnArrayClassMethodToYieldRector::class,
+            [new ReturnArrayClassMethodToYield(TestCase::class, '*provide*')]
+        );
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
+    $rectorConfig->bootstrapFiles([
+        __DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php',
+        __DIR__ . '/vendor/symplify/easy-coding-standard/vendor/autoload.php',
+        __DIR__ . '/vendor/nunomaduro/larastan/bootstrap.php',
+    ]);
+    $rectorConfig->skip([
+        RenameParamToMatchTypeRector::class,
+        AddSeeTestAnnotationRector::class,
+        FinalizeClassesWithoutChildrenRector::class,
+    ]);
+    $rectorConfig->paths(
         [__DIR__ . '/packages', __DIR__ . '/ecs.php', __DIR__ . '/monorepo-builder.php', __DIR__ . '/rector.php']
     );
 };
