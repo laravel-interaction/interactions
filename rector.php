9f92bee00d36a3bd22bd21eab7f5de8f945318fd
zingimmick <zingimmick@users.noreply.github.com>
2022-05-08 13:11:39 +0000

    Rector Rectify

--- rector.php
+++ rector.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use Rector\CodingStyle\Rector\ClassMethod\ReturnArrayClassMethodToYieldRector;
 use Rector\CodingStyle\ValueObject\ReturnArrayClassMethodToYield;
-use Rector\Core\Configuration\Option;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Zing\CodingStandard\Set\RectorSetList;
 
-return static function (\Rector\Config\RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([LevelSetList::UP_TO_PHP_73, PHPUnitSetList::PHPUNIT_CODE_QUALITY,RectorSetList::CUSTOM ]);
+return static function (Rector\Config\RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_73, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
     $rectorConfig
-        ->ruleWithConfiguration(ReturnArrayClassMethodToYieldRector::class, [new ReturnArrayClassMethodToYield(TestCase::class, '*provide*')]);
+        ->ruleWithConfiguration(
+            ReturnArrayClassMethodToYieldRector::class,
+            [new ReturnArrayClassMethodToYield(TestCase::class, '*provide*')]
+        );
     $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
     $rectorConfig->bootstrapFiles([
         __DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php',
@@ -29,5 +30,7 @@ return static function (\Rector\Config\RectorConfig $rectorConfig): void {
         AddSeeTestAnnotationRector::class,
         FinalizeClassesWithoutChildrenRector::class,
     ]);
-    $rectorConfig->paths([__DIR__ . '/packages', __DIR__ . '/ecs.php', __DIR__ . '/monorepo-builder.php', __DIR__ . '/rector.php']);
+    $rectorConfig->paths(
+        [__DIR__ . '/packages', __DIR__ . '/ecs.php', __DIR__ . '/monorepo-builder.php', __DIR__ . '/rector.php']
+    );
 };
