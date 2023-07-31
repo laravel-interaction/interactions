a7b2df06ebc6c8684608169fbf0bd2a006f23975
Zing <zingimmick@outlook.com>
2023-07-31 10:29:54 +0800

    Update ecs.php

--- ecs.php
+++ ecs.php
@@ -8,14 +8,12 @@ use Zing\CodingStandard\Set\ECSSetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_80, ECSSetList::CUSTOM]);
-    $parameters = $ecsConfig->parameters();
     $ecsConfig->parallel();
     $ecsConfig->skip([
         \PHP_CodeSniffer\Standards\PSR1\Sniffs\Classes\ClassDeclarationSniff::class => ['*/migrations/*'],
         \PHP_CodeSniffer\Standards\Squiz\Sniffs\PHP\CommentedOutCodeSniff::class,
     ]);
-    $parameters->set(
-        Option::PATHS,
+    $ecsConfig->paths(
         [__DIR__ . '/packages', __DIR__ . '/ecs.php', __DIR__ . '/monorepo-builder.php', __DIR__ . '/rector.php']
     );
 };
