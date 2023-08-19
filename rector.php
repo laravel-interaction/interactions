56ce156fc571d3d782acc4116287db7cbf2bc62f
Zing <zingimmick@outlook.com>
2023-08-19 09:20:05 +0800

    Rename skipped rules on $rectorConfig->skip() are changed to different namespace  (#160)

--- rector.php
+++ rector.php
@@ -2,12 +2,8 @@
 
 declare(strict_types=1);
 
-use PHPUnit\Framework\TestCase;
-use Rector\CodingStyle\Rector\ClassMethod\ReturnArrayClassMethodToYieldRector;
-use Rector\CodingStyle\ValueObject\ReturnArrayClassMethodToYield;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
-use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
-use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\LevelSetList;
@@ -15,11 +11,6 @@ use Zing\CodingStandard\Set\RectorSetList;
 
 return static function (Rector\Config\RectorConfig $rectorConfig): void {
     $rectorConfig->sets([LevelSetList::UP_TO_PHP_80, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
-    $rectorConfig
-        ->ruleWithConfiguration(
-            ReturnArrayClassMethodToYieldRector::class,
-            [new ReturnArrayClassMethodToYield(TestCase::class, '*provide*')]
-        );
     $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
     $rectorConfig->bootstrapFiles([
         __DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php',
@@ -30,7 +21,6 @@ return static function (Rector\Config\RectorConfig $rectorConfig): void {
         RenameParamToMatchTypeRector::class,
         AddSeeTestAnnotationRector::class,
         FinalizeClassesWithoutChildrenRector::class,
-        UnionTypesRector::class,
     ]);
     $rectorConfig->paths(
         [__DIR__ . '/packages', __DIR__ . '/ecs.php', __DIR__ . '/monorepo-builder.php', __DIR__ . '/rector.php']
