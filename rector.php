8b146bce7e55b9b606867acc293a15545fa48927
Zing <zingimmick@outlook.com>
2023-08-19 09:15:58 +0800

    Update rector.php

--- rector.php
+++ rector.php
@@ -15,11 +15,6 @@ use Zing\CodingStandard\Set\RectorSetList;
 
 return static function (Rector\Config\RectorConfig $rectorConfig): void {
     $rectorConfig->sets([LevelSetList::UP_TO_PHP_80, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
-    $rectorConfig
-        ->ruleWithConfiguration(
-            ReturnArrayClassMethodToYieldRector::class,
-            [new ReturnArrayClassMethodToYield(TestCase::class, '*provide*')]
-        );
     $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
     $rectorConfig->bootstrapFiles([
         __DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php',
@@ -30,7 +25,6 @@ return static function (Rector\Config\RectorConfig $rectorConfig): void {
         RenameParamToMatchTypeRector::class,
         AddSeeTestAnnotationRector::class,
         FinalizeClassesWithoutChildrenRector::class,
-        UnionTypesRector::class,
     ]);
     $rectorConfig->paths(
         [__DIR__ . '/packages', __DIR__ . '/ecs.php', __DIR__ . '/monorepo-builder.php', __DIR__ . '/rector.php']
